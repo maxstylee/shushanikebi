10519e089b5a5dd93fc3b2502473272a758b1414
Giorgi Egnatashvili <egnatashvili.gio@gmail.com>
2026-03-04 13:59:12 +0400

    feat: add javascript logic on homme page. fix little structure from index.html to example.html

---
+++ card-data.js
@@ -0,0 +1,301 @@
+// MADE FOR U data start here
+let musicData = [
+  {
+    id: "music1",
+    title: "Discover Weekly",
+    author: "Your weekly mixtape of fresh music.",
+    number: "50",
+    color: "rgba(206, 110, 154, 1)",
+    image: "../asset/images/discover weekly.svg",
+  },
+  {
+    id: "music2",
+    title: "Daily Mix 1",
+    author: "Linkin Park, System Of A Down, Coal Chamber...",
+    number: "50",
+    color: "rgba(149, 230, 211, 1)",
+    image: "../asset/images/daily mix 1.svg",
+  },
+  {
+    id: "music3",
+    title: "Daily Mix 2",
+    author: "Avril Lavigne, Lorde, Charli XCX and more",
+    number: "50",
+    color: "rgba(244, 184, 189, 1)",
+    image: "../asset/images/daily mix 2.svg",
+  },
+  {
+    id: "music4",
+    title: "Daily Mix 3",
+    author: "The Strokes, Martin Garrix, MGMT and more",
+    number: "50",
+    color: "rgba(195, 231, 183, 1)",
+    image: "../asset/images/daily mix 3.svg",
+  },
+  {
+    id: "music5",
+    title: "Daily Mix 4",
+    author: "Chuck Berry, Elvis Presley, Roy Orbison and more",
+    number: "50",
+    color: "rgba(243, 127, 166, 1)",
+    image: "../asset/images/daily mix 4.svg",
+  },
+  {
+    id: "music6",
+    title: "Daily Mix 5",
+    author: "FrankSinatra, Gerhard Trede, Dean Martin and more",
+    number: "50",
+    color: "rgba(160, 190, 200, 1)",
+    image: "../asset/images/daily mix 5.svg",
+  },
+  {
+    id: "music7",
+    title: "Nirvana Radio",
+    author: "The Strokes, Martin Garrix, MGMT and more",
+    number: "50",
+    color: "rgba(253, 216, 125, 1)",
+    image: "../asset/images/nirvana.svg",
+  },
+  {
+    id: "music8",
+    title: "Fall Out Boy",
+    author: "Red Hot Chili Peppers, R.E.M., Guns N' Roses...",
+    number: "50",
+    color: "rgb(163, 230, 53)",
+    image: "../asset/images/fall-out-boy.svg",
+  },
+  {
+    id: "music9",
+    title: "Adele Radio",
+    author: "With Sia, Sam Smith, P!nk and more",
+    number: "50",
+    color: "rgba(222, 176, 117, 1)",
+    image: "../asset/images/Adele.svg",
+  },
+  {
+    id: "music10",
+    title: "Daft Punk Radio",
+    author: "With Justice, The Chemical Brothers, ...",
+    number: "50",
+    color: "rgba(141, 195, 181, 1)",
+    image: "../asset/images/Daft Punk.svg",
+  },
+  {
+    id: "music11",
+    title: "ABBA Radio",
+    author: "With Honey M., Bee Gees, Bonnie Tyler, and more",
+    number: "50",
+    color: "rgba(166, 186, 211, 1)",
+    image: "../asset/images/ABBA.svg",
+  },
+]; // MADE FOR U data finished here
+
+// YOUR TOP MIXES data start here
+let musicDataMix = [
+  {
+    id: "music12",
+    title: "Rock Mix",
+    author: "MF DOOM, Daft Punk, The Chainsmokers, and more",
+    number: "50",
+    color: "rgb(253, 224, 71)",
+    image: "../asset/images/rock-mix.svg",
+  },
+
+  {
+    id: "music13",
+    title: "Chill Mix",
+    author: "Red Hot Chili Peppers, R.E.M., Guns N' Roses...",
+    number: "50",
+    color: "rgb(103, 232, 249)",
+    image: "../asset/images/chill-mix.svg",
+  },
+  {
+    id: "music14",
+    number: "50",
+    image: "../asset/images/pop-mix.svg",
+    title: "Pop Mix",
+    color: "rgb(139, 92, 246)",
+    author: "Daft Punk, MGMT, Muse, and more",
+  },
+  {
+    id: "music15",
+    number: "50",
+    image: "../asset/images/Daft-Punk-Mix.svg",
+    title: "Daft Punk Mix",
+    color: "rgb(249, 168, 212)",
+    author: "The Turtles, Van Morrison, ABBA and more",
+  },
+  {
+    id: "music16",
+    number: "50",
+    image: "../asset/images/Happy-Mix.svg",
+    title: "Happy Mix",
+    color: "rgb(253, 186, 116)",
+    author: "Soft Cell, The Who and The Clash",
+  },
+  {
+    id: "music17",
+    number: "50",
+    image: "../asset/images/David-Bowie-Mix.svg",
+    title: "David Bowie Mix",
+    color: "rgb(52, 211, 153)",
+    author: "Kylie Minogue, Charli XCX, Cage The Elephant...",
+  },
+  {
+    id: "music18",
+    number: "50",
+    image: "../asset/images/Upbeat-Mix.svg",
+    title: "Upbeat Mix",
+    color: "rgb(255, 77, 166)",
+    author: "Chuck Berry, Skeeter Davis, Van Morrison and more",
+  },
+  {
+    id: "music19",
+    number: "50",
+    image: "../asset/images/60s-Mix.svg",
+    title: "60s Mix",
+    color: "rgba(221, 19, 139, 1)",
+    author: "Chuck Berry, Skeeter Davis, Van Morrison and more",
+  },
+]; // YOUR TOP MIXES sextion finish here
+
+// favorite artist data start here
+let favoriteArtist = [
+  {
+    image: "../asset/images/guns-roses.svg",
+    title: "Guns N' Roses",
+  },
+  {
+    image: "../asset/images/Daft-Punk.svg",
+    title: "Daft Punk",
+  },
+  {
+    image: "../asset/images/David-Bowie.svg",
+    title: "David Bowie",
+  },
+]; // favorite artist data finish here
+
+// audiobooks data start here
+let audioBooks = [
+  {
+    image: "../asset/images/halo1.svg",
+    title: "Halo: The Flood",
+    author: "William C. Dietz",
+  },
+  {
+    image: "../asset/images/halo2.svg",
+    title: "Halo: First Strike",
+    author: "Eric Nylund",
+  },
+  {
+    image: "../asset/images/halo3.svg",
+    title: "Halo: Ghosts of Onyx",
+    author: "Eric Nylund",
+  },
+]; // audiobooks data finish here
+
+// podcast data start here
+let podcasts = [
+  {
+    image: "../asset/images/anything-goes.svg",
+    title: "anything goes...",
+    author: "emma chamberlain",
+  },
+];
+
+let cardData = [
+  {
+    title: "Made For You",
+    color: "rgba(30, 50, 100, 1)",
+    image: "../asset/images/made for you.png",
+  },
+  {
+    title: "new releases",
+    color: "rgba(186, 93, 7, 1)",
+    image: "../asset/images/new releases.png",
+  },
+  {
+    title: "Spotify Classics",
+    color: "rgba(30, 50, 100, 1)",
+    image: "../asset/images/Spotify Classics.png",
+  },
+  {
+    title: "Charts",
+    color: "rgba(141, 103, 171, 1)",
+    image: "../asset/images/Charts.png",
+  },
+  {
+    title: "Trending",
+    color: "rgba(176, 40, 151, 1)",
+    image: "../asset/images/Trending.png",
+  },
+  {
+    title: "Discover",
+    color: "rgba(141, 103, 171, 1)",
+    image: "../asset/images/Discover.png",
+  },
+  {
+    title: "Spotify Singles",
+    color: "rgba(119, 119, 119, 1)",
+    image: "../asset/images/Spotify Singles.png",
+  },
+  {
+    title: "Decades",
+    color: "rgba(186, 93, 7, 1)",
+    image: "../asset/images/Decades.png",
+  },
+
+  //
+
+  {
+    title: "Pop",
+    color: "rgba(20, 138, 8, 1)",
+    image: "../asset/images/Pop.png",
+  },
+  {
+    title: "Country",
+    color: "rgba(216, 64, 0, 1)",
+    image: "../asset/images/Country.png",
+  },
+  {
+    title: "Hip-Hop",
+    color: "rgba(80, 55, 80, 1)",
+    image: "../asset/images/Hip-Hop.png",
+  },
+  {
+    title: "Rock",
+    color: "rgba(0, 100, 80, 1)",
+    image: "../asset/images/Rock.png",
+  },
+  {
+    title: "Indie",
+    color: "rgba(233, 20, 41, 1)",
+    image: "../asset/images/Indie.png",
+  },
+  {
+    title: "Punk",
+    color: "rgba(30, 50, 100, 1)",
+    image: "../asset/images/Punk.png",
+  },
+  {
+    title: "Metal",
+    color: "rgba(233, 20, 41, 1)",
+    image: "../asset/images/Metal.png",
+  },
+  {
+    title: "Instrumental",
+    color: "rgba(83, 122, 161, 1)",
+    image: "../asset/images/Instrumental.png",
+  },
+];
+
+
+
+export default{
+    musicData,
+    musicDataMix,
+    favoriteArtist,
+    audioBooks,
+    podcasts,
+    cardData,
+}
